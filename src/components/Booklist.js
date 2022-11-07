6d58a63f208bd4ee6f73243121f98037f0998e0d
stefan22 <stefanosemail@protonmail.com>
2022-11-07 15:05:27 +0000

    visits bookshop - shows Homer as one of titles pass

---
+++ src/components/Booklist.js
@@ -0,0 +1,15 @@
+const Booklist = ({ books }) => {
+  return (
+    <div data-test="book-list">
+      {
+        books.map(book => (
+          <div className="book-item">
+            <h2 className="title">{book.name}</h2>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Booklist
