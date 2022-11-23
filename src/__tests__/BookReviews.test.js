a81836f2ce51aae8d91f9d895c1a57c20d5ba2c1
Stefano L <stefanosemail@protonmail.com>
2022-11-23 17:40:39 +0000

    review jest tests

--- src/__tests__/BookReviews.test.js
+++ src/__tests__/BookReviews.test.js
@@ -1,11 +1,11 @@
 /*
  * @jest-environment jsdom
- * @jest-environment-options {"url": "https://jestjs.io/"}
  */
 import React from 'react'
 import { render } from '@testing-library/react'
 import BookReviewsContainer from '../layout/BookReviewsContainer'
 import BookDetails from '../components/BookDetails'
+import ReviewForm from '../components/ReviewForm'
 
 describe('Reviews', () => {
   it('renders no reviews, and "Be the first to review...." message', () => {
@@ -102,5 +102,21 @@ describe('Reviews', () => {
 
     expect(reviewContentTitle).toEqual(props.reviews[0].content)
     expect(reviewAuthor).toContain(props.reviews[0].name)
+  })
+
+  it('should render books review form', () => {
+    const { container } = render(<ReviewForm />)
+    const form = container.querySelector(
+      '[data-testid="books-review-form"]'
+    )
+    const input = container.querySelector(
+      '[data-testid="books-review-form-input"]'
+    )
+    const txtarea = container.querySelector(
+      '[data-testid="books-review-form-textarea"]'
+    )
+    expect(form).toBeInTheDocument()
+    expect(input).toBeInTheDocument()
+    expect(txtarea).toBeInTheDocument()
   })
 })
